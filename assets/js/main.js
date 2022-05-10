5c5bd14305019c4c88f337b16f77ad2c6a7dd5bf
Ilya Shutikov <DeliasZen@gmail.com>
2022-05-10 16:04:20 +0300

    feat: added basic page layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,62 @@
 const container = document.querySelector('#poke-container');
-const pokemonsNumber = 150;
+const pokemonsNumber = 50;
 
-const getPokemon = () =>
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5'
+}
 
-  console.log(container);
+const main_types = Object.keys(colors);
+
+const fetchPokemons = (pokemonsNumber) => {
+  for (let i = 1; i <= pokemonsNumber; i++) getPokemon(i)
+}
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+  const pokemon = await fetch(url).then(url => url.json())
+
+  createPokemonCard(pokemon)
+}
+
+function createPokemonCard(pokemon) {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const poke_types = pokemon.types.map(el => el.type.name);
+  const type = main_types.find(type => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokeInnerHTML = `
+  <div class="img-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+  </div>
+  <div class="info">
+    <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>
+  `;
+
+  pokemonEl.innerHTML = pokeInnerHTML;
+
+  container.appendChild(pokemonEl);
+}
+
+fetchPokemons(pokemonsNumber)
+// fetchPokemon()
